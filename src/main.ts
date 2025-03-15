9e3149d8312fed733d44daad7b51b6780450335e
Murilo <mbalves1@outlook.com>
2025-03-15 11:31:09 -0300

    [feat]: cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: ['http://localhost:3000', 'https://auth-v1-drps.vercel.app'],
     methods: 'GET,POST,PUT,DELETE',
     allowedHeaders: 'Content-Type, Authorization',
   });
